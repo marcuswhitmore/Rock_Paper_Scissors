1dd71cdd204fec350713dcfe7d45fa0e0a962397
Marcus Whitmore <marcus.whitmore@gmail.com>
2019-03-15 01:24:29 -0500

    cache dom

--- file.js
+++ file.js
@@ -8,3 +8,15 @@ const rock_div = document.getElementById("r");
 const paper_div = document.getElementById("p");
 const scissors_div = document.getElementById("s");
 
+rock_div.addEventListener('click', function() {
+    console.log("You clicked rock.");
+})
+
+paper_div.addEventListener('click', function () {
+    console.log("You clicked paper.");
+} )
+
+scissors_div.addEventListener('click', function () {
+    console.log("You clicked scissors.");
+} )
+
