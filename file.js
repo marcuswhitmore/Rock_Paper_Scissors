2c84476c1080e84a3b12dc6d2ff9633b8900cf8b
Marcus Whitmore <marcus.whitmore@gmail.com>
2019-04-17 16:57:42 -0500

    change firebase database

--- file.js
+++ file.js
@@ -1,3 +1,18 @@
+var config = {
+    apiKey: "AIzaSyCqfk1liildjvaIx98rXFp75sWjyv_NIV0",
+    authDomain: "rock-paper-scissors-29571.firebaseapp.com",
+    databaseURL: "https://rock-paper-scissors-29571.firebaseio.com",
+    projectId: "rock-paper-scissors-29571",
+    storageBucket: "",
+    messagingSenderId: "783611167588"
+  };
+  firebase.initializeApp(config);
+  
+  // Create a variable to reference the database.
+  var database = firebase.database();
+
+
+
 let userScore = 0;
 let computerScore = 0;
 const userScore_span = document.getElementById("user-score");
@@ -11,21 +26,39 @@ const scissors_div = document.getElementById("s");
 function getComputerChoice() {
     const choices = ["r", "p", "s"];
     const randomNumber = (Math.floor(Math.random()* 3));
+    
+    
+    
     return choices[randomNumber];
+
+    
+    
 } 
 
 function win(userChoice, computerChoice) {
     userScore++;
     userScore_span.innerHTML = userScore;
     computerScore_span.innerHTML = computerScore;
+
+    database.ref().set({
+        outcome: "win"
+    })
 }
 
 function draw() {
     console.log("draw")
+
+    database.ref().set({
+        outcome: "draw"
+    })
 }
 function lose() {
    computerScore++;
    computerScore_span.innerHTML = computerScore;
+
+   database.ref().set({
+       outcome: "lose"
+   })
 }
 
 
@@ -53,15 +86,37 @@ function game(userChoice) {
 function main() {
     rock_div.addEventListener('click', function() {
         game("r");
+
+        database.ref().set( {
+            userPick: "rock"
+            
+        })
+
+        // database.ref().set({
+        //     computerWins: computerScore,
+        // })
+    
     })
 
     paper_div.addEventListener('click', function () {
         game("p");
+        
+        database.ref().set({
+            userPick: "paper",
+        })
     } )
 
     scissors_div.addEventListener('click', function () {
         game("s");
+        
+        database.ref().set({
+            userPick: "scissors",
+        });
     } )
 }
 
+
+
 main();
+
+
